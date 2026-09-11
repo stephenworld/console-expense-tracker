965a62e8dd187ea1c074c8092d1ce8a19a844996
Stephen Adewale <adewalestephennifemi@gmail.com>
2026-09-11 16:11:15 +0100

    wip: history handling login under development

--- utils/handle_history.py
+++ utils/handle_history.py
@@ -1,4 +1,45 @@
-actions = ["Show all transactions", "Filter by category", "Filter by amount range", "Search by title"]
+from utils.clear_screen import clear_terminal
+actions = ["Show all expenses", "Filter by category", "Filter by amount range", "Search by title"]
+
+def fetch_expenses():
+  """
+  Fetch all expenses from the data.json file
+  """
+  import json
+  json_file = "data.json"
+
+  try:
+      with open(json_file, "r") as file:
+          data = json.load(file)
+  except FileNotFoundError:
+      data = {}
+
+  return data
+
+def show_all_expenses():
+  """
+  Show all expenses
+  """
+  clear_terminal()
+  print("Showing All Expenses:\n")
+  expenses = fetch_expenses()
+  print("-"*60)
+  print(f"{'ID':<5} {'Title':<25} {'Category':<25}")
+  print("-"*60)
+  for expense in expenses:
+    print(f"{expense:<5} {expenses[expense]['title']:<25} {expenses[expense]['category']:<25}")
+
+
+def show_expenses_by_category():
+  """
+  Show expenses filtered by category
+  """
+  clear_terminal()
+  print(f"Showing Expenses in Categorys\n")
+
+  all_expenses = fetch_expenses()
+  
+
 
 def handle_history():
   """
@@ -16,14 +57,16 @@ def handle_history():
 
   if user_action == "1":
     """
-    Show all transactions
+    Show all expenses
     """
-    print("Showing all transactions...")
+    show_all_expenses()
+
   elif user_action == "2":
     """
     Filter by category
     """
-    print("Filtering by category...")
+    show_expenses_by_category()
+
   elif user_action == "3":
     """
     Filter by amount range
